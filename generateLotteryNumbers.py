29acd361815512edbdff722b114c80ee80d8c82b
Chris Vrabel <cjv38@cornell.edu>
2022-05-19 12:23:56 -0700

    Updating team order for 2022

--- generateLotteryNumbers.py
+++ generateLotteryNumbers.py
@@ -1,40 +1,42 @@
 from random import randint
 import json
 
-def generateLotteryNumbers():
-	allChosenNumbersArray = []
-	firstPickNumbersArray = []
-	secondPickNumbersArray = []
-	thirdPickNumbersArray = []
-	fourthPickNumbersArray = []
-	fifthPickNumbersArray = []
 
-	allChosenNumbersArray, firstPickNumbersArray = fillArrayWithNumbers(allChosenNumbersArray, 430)
-	allChosenNumbersArray, secondPickNumbersArray = fillArrayWithNumbers(allChosenNumbersArray, 265)
-	allChosenNumbersArray, thirdPickNumbersArray = fillArrayWithNumbers(allChosenNumbersArray, 165)
-	allChosenNumbersArray, fourthPickNumbersArray = fillArrayWithNumbers(allChosenNumbersArray, 90)
-	allChosenNumbersArray, fifthPickNumbersArray = fillArrayWithNumbers(allChosenNumbersArray, 50)
+def generate_lottery_numbers():
+    allChosenNumbersArray = []
+    firstPickNumbersArray = []
+    secondPickNumbersArray = []
+    thirdPickNumbersArray = []
+    fourthPickNumbersArray = []
+    fifthPickNumbersArray = []
 
-	dumpNumbers([firstPickNumbersArray, secondPickNumbersArray, thirdPickNumbersArray, fourthPickNumbersArray, fifthPickNumbersArray])
+    allChosenNumbersArray, firstPickNumbersArray = fill_array_with_numbers(allChosenNumbersArray, 430)
+    allChosenNumbersArray, secondPickNumbersArray = fill_array_with_numbers(allChosenNumbersArray, 265)
+    allChosenNumbersArray, thirdPickNumbersArray = fill_array_with_numbers(allChosenNumbersArray, 165)
+    allChosenNumbersArray, fourthPickNumbersArray = fill_array_with_numbers(allChosenNumbersArray, 90)
+    allChosenNumbersArray, fifthPickNumbersArray = fill_array_with_numbers(allChosenNumbersArray, 50)
+
+    write_to_json([firstPickNumbersArray, secondPickNumbersArray, thirdPickNumbersArray,
+                 fourthPickNumbersArray, fifthPickNumbersArray])
 
 
+def fill_array_with_numbers(allChosenNumbersArray, n):
+    numbersArray = []
+    while len(numbersArray) < n:
+        randomNumber = randint(0, 999)
+        if randomNumber not in allChosenNumbersArray:
+            allChosenNumbersArray.append(randomNumber)
+            numbersArray.append(randomNumber)
+    numbersArray.sort()
+    return allChosenNumbersArray, numbersArray
 
-def fillArrayWithNumbers(allChosenNumbersArray, n):
-	numbersArray = []
-	while len(numbersArray) < n:
-		randomNumber = randint(0,999)
-		if randomNumber not in allChosenNumbersArray:
-			allChosenNumbersArray.append(randomNumber)
-			numbersArray.append(randomNumber)
-	numbersArray.sort()
-	return allChosenNumbersArray, numbersArray;
 
-def dumpNumbers(arrayOfArrays):
-	for i in range(5):
-		fileName = "pickNumbersArray" + str(i+1) + ".json"
-		with open(fileName, 'w') as fp:
-			json.dump(arrayOfArrays[i], fp)
+def write_to_json(arrayOfArrays):
+    for i in range(5):
+        fileName = "pickNumbersArray" + str(i + 1) + ".json"
+        with open(fileName, 'w') as fp:
+            json.dump(arrayOfArrays[i], fp)
 
 
 if __name__ == "__main__":
-	generateLotteryNumbers()
+    generate_lottery_numbers()
